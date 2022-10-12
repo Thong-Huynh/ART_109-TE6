dc71399edf38d1c85e1aa3304b766bfea69b245d
Thong-Huynh <minhthong.huynh@sjsu.edu>
2022-10-12 00:04:16 -0700

    upload exercise 3

--- Part-1/exercise-3.js
+++ Part-1/exercise-3.js
@@ -1,2 +1,25 @@
 // This is Exercise 3
 console.log("This is Exercise 3");
+
+let evenRow = "";
+let oddRow = "";
+let n = " #";
+let m = "# ";
+let l = 0;
+let k = 0;
+let j = 8;
+
+while (l < j / 2) {
+    l++;
+    oddRow += n;
+    evenRow += m;
+}
+
+while (k < j) {
+    k++;
+    if (k % 2 == 0) {
+        console.log(evenRow);
+    } else {
+        console.log(oddRow);
+    }
+}
